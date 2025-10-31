88d979390e095a968034b3f126b54bc3015e6f7a
SOHEL RANA <sohelrana104322@gmail.com>
2025-11-01 00:28:06 +0600

    create role validator

---
+++ server/src/validators/roleValidator.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+export const roleSchemaValidator = z.object({
+    name: z.string({ error: "Role name is required!" })
+        .min(1, "Role name must be at least 1 characters long")
+        .toLowerCase()
+        .trim(),
+
+    description: z.string({ error: "Description is required!" })
+        .min(1, "Description must be at least 1 characters long")
+        .toLowerCase(),
+
+    features: z.preprocess((arg) => (typeof arg === "string" ? arg.split(",") : arg),
+        z.array(z.string().min(1, "Feature must be required."))
+            .nonempty("At least one feature is required")),
+
+    createdBy: z.any().optional(),
+
+    isActive: z.boolean().optional().default(true)
+});
+
+// Validator for updating a user (all fields optional)
+export const updateRoleValidator = roleSchemaValidator.partial();
