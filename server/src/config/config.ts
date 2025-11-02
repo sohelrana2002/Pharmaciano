0f25e46ebf8fa1f84052282134e7b1da80e757bb
SOHEL RANA <sohelrana104322@gmail.com>
2025-11-02 20:49:44 +0600

    create config for better env variable

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -4,6 +4,8 @@ dotenvConfig();
 interface IConfig {
     port: number;
     databaseURI: string;
+    superAdminEmail: string;
+    superAdminPassword: string;
 }
 
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 5001;
@@ -11,10 +13,20 @@ const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 5001;
 if (!process.env.MONGOOSE_STRING_URI) {
     throw new Error("MONGOOSE_STRING_URI is required in environment variables");
 }
-
 const databaseURI = process.env.MONGOOSE_STRING_URI;
 
-const _config: IConfig = { port, databaseURI };
+if (!process.env.SUPER_ADMIN_EMAIL) {
+    throw new Error("SUPER ADMIN EMAIL is required in environment variables");
+}
+const superAdminEmail = process.env.SUPER_ADMIN_EMAIL;
+
+if (!process.env.SUPER_ADMIN_PASSWORD) {
+    throw new Error("SUPER ADMIN PASSWORD is required in environment variables");
+}
+const superAdminPassword = process.env.SUPER_ADMIN_PASSWORD;
+
+
+const _config: IConfig = { port, databaseURI, superAdminEmail, superAdminPassword };
 
 // freeze to prevent accidental modification
 export const config = Object.freeze(_config);
