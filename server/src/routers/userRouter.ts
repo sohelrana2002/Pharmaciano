747ae72e5279d381b5cde5126dfd35dda7aa7944
SOHEL RANA <sohelrana104322@gmail.com>
2025-11-03 21:19:55 +0600

    profile route

--- server/src/routers/userRouter.ts
+++ server/src/routers/userRouter.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createUser, userList } from '../controllers/userController';
+import { createUser, userList, userProfile } from '../controllers/userController';
 import { authenticate, isSuperAdmin } from '../middlewares/authMiddleware';
 import { createUserValidator } from '../validators/authValidator';
 import { validate } from '../middlewares/validateMiddleware';
@@ -9,5 +9,8 @@ const router = Router();
 
 router.post('/', authenticate, isSuperAdmin, validate(createUserValidator), createUser);
 router.get('/', authenticate, authorize(["user:read"]), userList);
+// router.put("/update", authenticate, authorize(["user:update"]), updateUser);
+// router.put("/update", authenticate, updateUser);
+router.get("/profile", authenticate, userProfile);
 
 export default router;
