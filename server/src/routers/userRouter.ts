30cd8f54f5d19f2882f2bbb0593a54d62a1a5e15
SOHEL RANA <sohelrana104322@gmail.com>
2025-11-04 21:24:17 +0600

    update user router

--- server/src/routers/userRouter.ts
+++ server/src/routers/userRouter.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
-import { createUser, userList, userProfile } from '../controllers/userController';
+import { createUser, userList, userProfile, updateUser } from '../controllers/userController';
 import { authenticate, isSuperAdmin } from '../middlewares/authMiddleware';
-import { createUserValidator } from '../validators/authValidator';
+import { createUserValidator, updateUserValidator } from '../validators/authValidator';
 import { validate } from '../middlewares/validateMiddleware';
 import { authorize } from '../middlewares/authMiddleware';
 
@@ -12,5 +12,8 @@ router.get('/', authenticate, authorize(["user:read"]), userList);
 // router.put("/update", authenticate, authorize(["user:update"]), updateUser);
 // router.put("/update", authenticate, updateUser);
 router.get("/profile", authenticate, userProfile);
+
+// update router 
+router.put("/:id", authenticate, isSuperAdmin, validate(updateUserValidator), updateUser)
 
 export default router;
