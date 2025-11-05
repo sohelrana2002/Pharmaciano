a9f1a1ca87731aeff9c20e57cf7db68b451b11f4
SOHEL RANA <sohelrana104322@gmail.com>
2025-11-05 21:04:47 +0600

    make everything dynamic

--- server/src/routers/userRouter.ts
+++ server/src/routers/userRouter.ts
@@ -1,19 +1,18 @@
 import { Router } from 'express';
 import { createUser, userList, userProfile, updateUser } from '../controllers/userController';
-import { authenticate, isSuperAdmin } from '../middlewares/authMiddleware';
+import { authenticate } from '../middlewares/authMiddleware';
 import { createUserValidator, updateUserValidator } from '../validators/authValidator';
 import { validate } from '../middlewares/validateMiddleware';
 import { authorize } from '../middlewares/authMiddleware';
 
 const router = Router();
 
-router.post('/', authenticate, isSuperAdmin, validate(createUserValidator), createUser);
+router.post('/', authenticate, authorize(["user:create"]), validate(createUserValidator), createUser);
 router.get('/', authenticate, authorize(["user:read"]), userList);
 // router.put("/update", authenticate, authorize(["user:update"]), updateUser);
-// router.put("/update", authenticate, updateUser);
 router.get("/profile", authenticate, userProfile);
 
 // update router 
-router.put("/:id", authenticate, isSuperAdmin, validate(updateUserValidator), updateUser)
+router.put("/:id", authenticate, authorize(["user:update"]), validate(updateUserValidator), updateUser)
 
 export default router;
