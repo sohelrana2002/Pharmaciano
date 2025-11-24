c51edeb49f7ee64142982424bbf6b338e7aab441
SOHEL RANA <sohelrana104322@gmail.com>
2025-11-25 02:35:24 +0600

    authorize for user individual profile

--- server/src/routers/authRouter.ts
+++ server/src/routers/authRouter.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
 import { login, getProfile } from '../controllers/authController';
-import { authenticate } from '../middlewares/authMiddleware';
+import { authenticate, authorize } from '../middlewares/authMiddleware';
 
 const router = Router();
 
 router.post('/login', login);
-router.get('/profile', authenticate, getProfile);
+router.get('/profile', authenticate, authorize(["user:read"]), getProfile);
 
 export default router;
