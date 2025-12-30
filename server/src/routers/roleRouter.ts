9d0f10fa162d5cc189d84130ad1273772b41f6b8
SOHEL RANA <sohelrana104322@gmail.com>
2025-12-30 21:37:29 +0600

    added comment

--- server/src/routers/roleRouter.ts
+++ server/src/routers/roleRouter.ts
@@ -1,16 +1,18 @@
 import { Router } from 'express';
 import { createRole, getRoles, getFeatures } from '../controllers/roleController';
-import { authenticate, isSuperAdmin } from '../middlewares/authMiddleware';
+import { authenticate, authorize } from '../middlewares/authMiddleware';
 import { validate } from '../middlewares/validateMiddleware';
 import { roleSchemaValidator } from '../validators/roleValidator';
 
 const router = Router();
 
-router.use(authenticate);
-router.use(isSuperAdmin);
+// create role 
+router.post('/', authenticate, authorize(["role:create"]), validate(roleSchemaValidator), createRole);
 
-router.post('/', validate(roleSchemaValidator), createRole);
+// get list of role 
 router.get('/', getRoles);
+
+// get all features 
 router.get('/features', getFeatures);
 
 export default router;
