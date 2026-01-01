5b464d3a8778339c2fd7f75765baa4ac0067e30f
SOHEL RANA <sohelrana104322@gmail.com>
2026-01-01 22:55:29 +0600

    create delete role router

--- server/src/routers/roleRouter.ts
+++ server/src/routers/roleRouter.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { createRole, roleList, getFeatures, updateRole } from '../controllers/roleController';
+import { createRole, roleList, getFeatures, updateRole, deleteRole } from '../controllers/roleController';
 import { authenticate, authorize } from '../middlewares/authMiddleware';
 import { validate } from '../middlewares/validateMiddleware';
 import { roleSchemaValidator, updateRoleValidator } from '../validators/roleValidator';
@@ -15,6 +15,8 @@ router.get('/', authenticate, authorize(["role:list"]), roleList);
 // update role 
 router.put("/:id", authenticate, authorize(["role:update"]), validate(updateRoleValidator), updateRole);
 
+// delete role 
+router.delete("/:id", authenticate, authorize(["role:delete"]), deleteRole)
 
 // get all features 
 router.get('/features', getFeatures);
