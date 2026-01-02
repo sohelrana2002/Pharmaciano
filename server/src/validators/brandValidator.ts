ed7aefeec197f5f84a76aae8e3d1d8eb5ddfd5a2
SOHEL RANA <sohelrana104322@gmail.com>
2026-01-02 22:22:03 +0600

    brand validator

---
+++ server/src/validators/brandValidator.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+
+export const brandSchemaValidator = z.object({
+    name: z.string({ error: "Brand name is required." })
+        .min(1, "Brand name must be at least 1 characters long")
+        .toLowerCase()
+        .trim(),
+
+    manufacturer: z.string({ error: "Manufacturer is required." })
+        .min(1, "Manufacturer must be at least 1 characters long")
+        .toLowerCase(),
+
+    country: z.string({ error: "Country name is required." })
+        .min(1, "Country name must be at least 1 characters long")
+        .toLowerCase(),
+});
+
+// Validator for updating a brand (all fields optional)
+export const updateBrandValidator = brandSchemaValidator.partial();
