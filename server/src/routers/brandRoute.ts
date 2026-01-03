e410ba240ad359c4ae071e20f67eacd23fba1587
SOHEL RANA <sohelrana104322@gmail.com>
2026-01-03 21:53:15 +0600

    individual brand info router

--- server/src/routers/brandRoute.ts
+++ server/src/routers/brandRoute.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createBrand, brandList } from "../controllers/brandController";
+import { createBrand, brandList, brandInfo } from "../controllers/brandController";
 import { validate } from "../middlewares/validateMiddleware";
 import { brandSchemaValidator } from "../validators/brandValidator";
 import { authenticate, authorize } from "../middlewares/authMiddleware";
@@ -11,5 +11,8 @@ router.post("/", authenticate, authorize(["inventory:manage"]), validate(brandSc
 
 // list of brand 
 router.get("/", authenticate, authorize(["inventory:manage"]), brandList);
+
+// individual brand info 
+router.get("/:id", authenticate, authorize(["inventory:manage"]), brandInfo)
 
 export default router;
