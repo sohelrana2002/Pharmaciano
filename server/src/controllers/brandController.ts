f6ae6b94402a2fd72bee4bd7238011725471ebdf
SOHEL RANA <sohelrana104322@gmail.com>
2026-01-04 21:49:49 +0600

    create brand update controller

--- server/src/controllers/brandController.ts
+++ server/src/controllers/brandController.ts
@@ -2,7 +2,8 @@
 import { AuthRequest } from "../types";
 import { Response } from "express";
 import Brand from "../models/BrandModel";
-import { brandSchemaValidator } from "../validators/brandValidator";
+import { brandSchemaValidator, updateBrandValidator } from "../validators/brandValidator";
+import mongoose from "mongoose";
 
 // create brand
 const createBrand = async (req: AuthRequest, res: Response) => {
@@ -110,4 +111,80 @@ const brandInfo = async (req: AuthRequest, res: Response) => {
     }
 }
 
-export { createBrand, brandList, brandInfo }
+// update brand 
+const updateBrand = async (req: AuthRequest, res: Response) => {
+    try {
+        const { id } = req.params;
+
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(400).json({
+                success: false,
+                message: "Invalid role ID."
+            })
+        }
+        // Validate request body using Zod
+        const validationResult = updateBrandValidator.safeParse(req.body);
+
+        if (!validationResult.success) {
+            return res.status(400).json({
+                success: false,
+                message: 'Validation failed',
+                errors: validationResult.error.issues.map((err: { path: any[]; message: any; }) => ({
+                    field: err.path.join('.'),
+                    message: err.message
+                }))
+            });
+        }
+
+        const { name, manufacturer, country } = validationResult.data;
+
+        const brand = await Brand.findOne({ _id: id });
+
+        if (!brand) {
+            return res.status(404).json({
+                success: false,
+                message: "Brand not found."
+            })
+        }
+
+        // prevent duplicate brand name 
+        if (name && name !== brand.name) {
+            const existingBrand = await Brand.findOne({ name });
+
+            if (existingBrand) {
+                return res.status(409).json({
+                    success: false,
+                    message: "Brand name already exist."
+                })
+            }
+        }
+
+        const updateData: any = {};
+
+        if (name) updateData.name = name;
+        if (manufacturer) updateData.manufacturer = manufacturer;
+        if (country) updateData.country = country;
+
+        // update brand 
+        const updateResult = await Brand.findByIdAndUpdate(
+            id,
+            updateData,
+            { new: true }
+        );
+
+        return res.status(200).json({
+            success: true,
+            message: "Brand update successfully.",
+            data: { updateResult }
+        })
+    } catch (error) {
+        console.error('Get profile error:', error);
+
+        res.status(500).json({
+            success: false,
+            message: 'Internal server error.'
+        });
+    }
+}
+
+export { createBrand, brandList, brandInfo, updateBrand }
