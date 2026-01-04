cb7ef516a0a784c79e5b3cbda8ae52665f1695af
SOHEL RANA <sohelrana104322@gmail.com>
2026-01-04 22:11:00 +0600

    create delete brand router

--- server/src/routers/brandRoute.ts
+++ server/src/routers/brandRoute.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createBrand, brandList, brandInfo, updateBrand } from "../controllers/brandController";
+import { createBrand, brandList, brandInfo, updateBrand, deleteBrand } from "../controllers/brandController";
 import { validate } from "../middlewares/validateMiddleware";
 import { brandSchemaValidator, updateBrandValidator } from "../validators/brandValidator";
 import { authenticate, authorize } from "../middlewares/authMiddleware";
@@ -17,5 +17,8 @@ router.get("/:id", authenticate, authorize(["inventory:manage"]), brandInfo);
 
 // update brand 
 router.put("/:id", authenticate, authorize(["inventory:manage"]), validate(updateBrandValidator), updateBrand);
+
+// delete brand 
+router.delete("/:id", authenticate, authorize(["inventory:manage"]), deleteBrand);
 
 export default router;
