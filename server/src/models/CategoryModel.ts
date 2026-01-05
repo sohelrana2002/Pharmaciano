a2d62f36f33ec66cc84a52af466fdb36d66fc5a0
SOHEL RANA <sohelrana104322@gmail.com>
2026-01-05 22:12:38 +0600

    category model

---
+++ server/src/models/CategoryModel.ts
@@ -0,0 +1,33 @@
+import { Schema, model } from "mongoose";
+import { ICategory } from "../types";
+
+const categorySchema = new Schema<ICategory>(
+    {
+        name: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            uppercase: true,
+        },
+
+        description: {
+            type: String,
+            trim: true,
+        },
+
+        isActive: {
+            type: Boolean,
+            default: true,
+        },
+
+        createdBy: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export default model<ICategory>("Category", categorySchema);
