51946d5cdf993386ad49fd6da2d5bb3a82bf8b74
SOHEL RANA <sohelrana104322@gmail.com>
2026-01-08 22:25:52 +0600

    create individual organization info router

--- server/src/routers/organizationRouter.ts
+++ server/src/routers/organizationRouter.ts
@@ -2,12 +2,15 @@ import { Router } from "express";
 import { authenticate, authorize } from "../middlewares/authMiddleware";
 import { validate } from "../middlewares/validateMiddleware";
 import { createOrganizationValidator } from "../validators/organizationValidator";
-import { createOrganization } from "../controllers/organizationController";
+import { createOrganization, organizationInfo } from "../controllers/organizationController";
 
 
 const router = Router();
 
 // create organization 
 router.post("/", authenticate, authorize(["organization:manage"]), validate(createOrganizationValidator), createOrganization);
+
+// individual organization details 
+router.get("/:id", authenticate, authorize(["organization:manage"]), organizationInfo);
 
 export default router;
