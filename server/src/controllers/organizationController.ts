e27b14a7e20e8752f161031e25b92e9331f3d86c
SOHEL RANA <sohelrana104322@gmail.com>
2026-01-10 22:58:55 +0600

    handle unique key error

--- server/src/controllers/organizationController.ts
+++ server/src/controllers/organizationController.ts
@@ -66,7 +66,23 @@ const createOrganization = async (req: AuthRequest, res: Response) => {
       message: "Organization created successfully.",
       data: { organization },
     });
-  } catch (error) {
+  } catch (error: any) {
+    //MongoDB Duplicate Key Error
+    if (error.code === 11000) {
+      const field = Object.keys(error.keyValue)[0];
+      const value = error.keyValue[field];
+
+      return res.status(409).json({
+        success: false,
+        message: `${value} already exists`,
+        error: {
+          field,
+          value,
+          reason: `${field} already exists`,
+        },
+      });
+    }
+
     console.error("Create organization error:", error);
 
     res.status(500).json({
