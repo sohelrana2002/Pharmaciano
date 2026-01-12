0db3a0361937aab3d63874c52ec41a732039110f
SOHEL RANA <sohelrana104322@gmail.com>
2026-01-12 23:03:27 +0600

    branch model

---
+++ server/src/models/BranchModel.ts
@@ -0,0 +1,47 @@
+import { Schema, model } from "mongoose";
+import { IBranch } from "../types";
+
+const BranchSchema = new Schema<IBranch>(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+
+    address: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+
+    organization: {
+      type: Schema.Types.ObjectId,
+      ref: "Organization",
+      required: true,
+    },
+
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default model<IBranch>("Branch", BranchSchema);
