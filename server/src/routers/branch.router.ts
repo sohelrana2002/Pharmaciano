ac315d6cfec6ab3947d52e6c2fc8b42506192206
SOHEL RANA <sohelrana104322@gmail.com>
2026-01-16 15:47:22 +0600

    create branch router

--- server/src/routers/branch.router.ts
+++ server/src/routers/branch.router.ts
@@ -1,5 +1,18 @@
 import { Router } from "express";
 import { validate } from "../middlewares/validate.middleware";
 import { branchSchemaValidator } from "../validators/branch.validator";
+import { authenticate, authorize } from "../middlewares/auth.middleware";
+import { createBranch } from "../controllers/branch.controller";
 
 const router = Router();
+
+// create branch
+router.post(
+  "/",
+  authenticate,
+  authorize(["organization:manage"]),
+  validate(branchSchemaValidator),
+  createBranch
+);
+
+export default router;
