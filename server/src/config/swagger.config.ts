d8fe2c790284e2564d26eb0ba9890f1a5ecf892a
SOHEL RANA <sohelrana104322@gmail.com>
2026-01-25 22:14:00 +0600

    swagger configaration

---
+++ server/src/config/swagger.config.ts
@@ -0,0 +1,48 @@
+import { config } from "./config";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Pharmaciano API",
+      version: "1.0.0",
+      description: `
+Pharmaciano is a comprehensive pharmaceutical management platform that allows
+administrators, organizations, and branch managers to manage users, roles,
+organizations, branches, brands, and subscriptions seamlessly. It supports
+feature-based access control, secure JWT authentication, and provides
+APIs for managing all entities in the system.
+      `,
+    },
+    servers: [
+      {
+        url: "http://localhost:5000", // Base URL for local development
+        description: "Local server",
+      },
+      {
+        url: `${config.backEndBaseUrl}`, // Replace with your production URL
+        description: "Production server",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "apiKey",
+          in: "header",
+          name: "Authorization",
+          description:
+            "JWT token WITHOUT 'Bearer ' prefix. Just the token itself.",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+
+  apis: ["./src/**/*.ts"], // files containing annotations
+};
+
+export default options;
