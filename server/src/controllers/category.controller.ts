552b703ef369bfd33f81232350e1c1405feb3964
SOHEL RANA <sohelrana104322@gmail.com>
2026-02-01 00:21:32 +0600

    update category route controller

--- server/src/controllers/category.controller.ts
+++ server/src/controllers/category.controller.ts
@@ -1,7 +1,10 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { AuthRequest } from "../types";
 import { Response } from "express";
-import { categorySchemaValidator } from "../validators/category.validator";
+import {
+  categorySchemaValidator,
+  updateCategoryValidator,
+} from "../validators/category.validator";
 import Category from "../models/Category.model";
 import mongoose from "mongoose";
 
@@ -138,4 +141,92 @@ const categoryInfo = async (req: AuthRequest, res: Response) => {
   }
 };
 
-export { createCategory, categoryList, categoryInfo };
+// category update
+const updateCategory = async (req: AuthRequest, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(409).json({
+        success: false,
+        message: "Invalid category ID!",
+      });
+    }
+
+    const validationResult = updateCategoryValidator.safeParse(req.body);
+
+    if (!validationResult.success) {
+      return res.status(400).json({
+        success: false,
+        message: "Validation failed",
+        errors: validationResult.error.issues.map(
+          (err: { path: any[]; message: any }) => ({
+            field: err.path.join("."),
+            message: err.message,
+          }),
+        ),
+      });
+    }
+
+    const { name, description } = validationResult.data;
+
+    const category = await Category.findById(id);
+
+    if (!category) {
+      return res.status(404).json({
+        success: false,
+        message: "Category not found!",
+      });
+    }
+
+    if (name && name !== category.name) {
+      const existingName = await Category.findOne({ name });
+
+      if (existingName) {
+        return res.status(409).json({
+          success: false,
+          message: "Category name alreday exists.",
+        });
+      }
+    }
+
+    const updateData: any = {};
+
+    if (name) updateData.name = name;
+    if (description) updateData.description = description;
+
+    const updateResult = await Category.findByIdAndUpdate(id, updateData, {
+      new: true,
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: "Category updated successfully!",
+      id: updateResult!._id,
+    });
+  } catch (error: any) {
+    //MongoDB Duplicate Key Error
+    if (error.code === 11000) {
+      const field = Object.keys(error.keyValue)[0];
+      const value = error.keyValue[field];
+
+      return res.status(409).json({
+        success: false,
+        message: `${value} already exists`,
+        error: {
+          field,
+          value,
+          reason: `${field} already exists`,
+        },
+      });
+    }
+    console.error("update categories error:", error);
+
+    res.status(500).json({
+      success: false,
+      message: "Internal server error.",
+    });
+  }
+};
+
+export { createCategory, categoryList, categoryInfo, updateCategory };
