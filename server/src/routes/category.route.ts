b95defb5e876d84ede4bdebe82e4d94491caf898
SOHEL RANA <sohelrana104322@gmail.com>
2026-02-01 00:21:25 +0600

    update category route

--- server/src/routes/category.route.ts
+++ server/src/routes/category.route.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { authenticate, authorize } from "../middlewares/auth.middleware";
 import { validate } from "../middlewares/validate.middleware";
-import { categorySchemaValidator } from "../validators/category.validator";
+import {
+  categorySchemaValidator,
+  updateCategoryValidator,
+} from "../validators/category.validator";
 import {
   createCategory,
   categoryList,
   categoryInfo,
+  updateCategory,
 } from "../controllers/category.controller";
 
 const router = Router();
@@ -24,5 +28,14 @@ router.get("/", authenticate, authorize(["inventory:manage"]), categoryList);
 
 // individual category info
 router.get("/:id", authenticate, authorize(["inventory:manage"]), categoryInfo);
+
+// update category
+router.put(
+  "/:id",
+  authenticate,
+  authorize(["inventory:manage"]),
+  validate(updateCategoryValidator),
+  updateCategory,
+);
 
 export default router;
