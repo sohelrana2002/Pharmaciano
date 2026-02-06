f12acb466d1f0d594d4f749f8e804fab94b5b5f3
SOHEL RANA <sohelrana104322@gmail.com>
2026-02-06 22:00:32 +0600

    update medicine route

--- server/src/routes/medicine.route.ts
+++ server/src/routes/medicine.route.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { authenticate, authorize } from "../middlewares/auth.middleware";
 import { validate } from "../middlewares/validate.middleware";
-import { medicineSchemaValidator } from "../validators/medicine.validator";
+import {
+  medicineSchemaValidator,
+  updateMedicineValidator,
+} from "../validators/medicine.validator";
 import {
   createMedicine,
   medicineList,
   medicineInfo,
+  updateMedicine,
   deleteMedicine,
 } from "../controllers/medicine.controller";
 
@@ -25,6 +29,15 @@ router.get("/", authenticate, authorize(["inventory:manage"]), medicineList);
 
 // individual medicine info
 router.get("/:id", authenticate, authorize(["inventory:manage"]), medicineInfo);
+
+// update medicine
+router.put(
+  "/:id",
+  authenticate,
+  authorize(["inventory:manage"]),
+  validate(updateMedicineValidator),
+  updateMedicine,
+);
 
 // delete medicine
 router.delete(
