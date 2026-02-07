8a5160c209a623552e615ad1163dae1b650833f4
SOHEL RANA <sohelrana104322@gmail.com>
2026-02-08 01:29:17 +0600

    create warehouse route

---
+++ server/src/routes/warehouse.route.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { authenticate, authorize } from "../middlewares/auth.middleware";
+import { validate } from "../middlewares/validate.middleware";
+import { warehousSchemaValidator } from "../validators/warehouse.validator";
+import { createWarehouse } from "../controllers/warehouse.controller";
+
+const router = Router();
+
+// create warehouse
+router.post(
+  "/",
+  authenticate,
+  authorize(["warehouse:manage"]),
+  validate(warehousSchemaValidator),
+  createWarehouse,
+);
+
+export default router;
