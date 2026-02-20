8f9558f9e006f99ce86fd5c50585bbce22f53974
SOHEL RANA <sohelrana104322@gmail.com>
2026-02-20 21:32:53 +0600

    update warehouse route

--- server/src/routes/warehouse.route.ts
+++ server/src/routes/warehouse.route.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { authenticate, authorize } from "../middlewares/auth.middleware";
 import { validate } from "../middlewares/validate.middleware";
-import { warehousSchemaValidator } from "../validators/warehouse.validator";
+import {
+  warehousSchemaValidator,
+  updateWarehouseValidator,
+} from "../validators/warehouse.validator";
 import {
   createWarehouse,
   warehouseList,
   warehouseInfo,
+  updateWarehouse,
   deleteWarehouse,
 } from "../controllers/warehouse.controller";
 
@@ -29,6 +33,15 @@ router.get(
   authenticate,
   authorize(["warehouse:manage"]),
   warehouseInfo,
+);
+
+// update warehouse
+router.put(
+  "/:id",
+  authenticate,
+  authorize(["warehouse:manage"]),
+  validate(updateWarehouseValidator),
+  updateWarehouse,
 );
 
 // delete warehouse
