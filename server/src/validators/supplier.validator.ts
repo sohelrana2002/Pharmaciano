ae532c6df7e63d95a893f7a83d5ef0d6332e79aa
SOHEL RANA <sohelrana104322@gmail.com>
2026-02-24 22:00:22 +0600

    supplier validator

---
+++ server/src/validators/supplier.validator.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+
+export const supplierSchemaValidator = z.object({
+  name: z
+    .string({ error: "Supplier name is required!" })
+    .min(1, "Supplier name must be at least 1 characters long")
+    .toLowerCase()
+    .trim(),
+  contactPerson: z
+    .string({ error: "Supplier contact person is required!" })
+    .min(1, "Supplier contact person must be at least 1 characters long")
+    .toLowerCase()
+    .trim(),
+  phone: z
+    .string({ error: "Phone number is required!" })
+    .regex(/^01\d{9}$/, "Phone number must start with 01 and be 11 digits"),
+  email: z
+    .string({ error: "Email is required!" })
+    .email({ error: "Invalid email address" }),
+  address: z
+    .string({ error: "Supplier address is required!" })
+    .min(1, "Supplier address must be at least 1 characters long")
+    .toLowerCase()
+    .trim(),
+});
+
+// Validator for updating a supplier (all fields optional)
+export const updateSupplierValidator = supplierSchemaValidator.partial();
