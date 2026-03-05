0cdd3279ba2bd9f396834fa5ba47b189c535094e
SOHEL RANA <sohelrana104322@gmail.com>
2026-03-06 02:54:42 +0600

    update inventory batch route

--- server/src/routes/inventoryBatch.route.ts
+++ server/src/routes/inventoryBatch.route.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { authenticate, authorize } from "../middlewares/auth.middleware";
 import { validate } from "../middlewares/validate.middleware";
-import { inventoryBatchSchemaValidator } from "../validators/inventoryBatch.validator";
+import {
+  inventoryBatchSchemaValidator,
+  updateInventoryBatchValidator,
+} from "../validators/inventoryBatch.validator";
 import {
   createInventoryBatch,
   inventoryBatchList,
   inventoryBatchInfo,
+  updateInventoryBatch,
 } from "../controllers/inventoryBatch.controller";
 
 const router = Router();
@@ -33,6 +37,15 @@ router.get(
   authenticate,
   authorize(["inventory:manage"]),
   inventoryBatchInfo,
+);
+
+// update inventoryBatch
+router.put(
+  "/:id",
+  authenticate,
+  authorize(["inventory:manage"]),
+  validate(updateInventoryBatchValidator),
+  updateInventoryBatch,
 );
 
 export default router;
