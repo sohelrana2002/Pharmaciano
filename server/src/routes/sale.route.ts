94462d8a648c9c27684cc57b843d457900d1c0b9
SOHEL RANA <sohelrana104322@gmail.com>
2026-03-21 23:21:33 +0600

    individual sale info route

--- server/src/routes/sale.route.ts
+++ server/src/routes/sale.route.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { authenticate, authorize } from "../middlewares/auth.middleware";
 import { validate } from "../middlewares/validate.middleware";
 import { saleSchemaValidator } from "../validators/sale.validator";
-import { createSale, saleList } from "../controllers/sale.controller";
+import { createSale, saleList, saleInfo } from "../controllers/sale.controller";
 
 const router = Router();
 
@@ -17,5 +17,8 @@ router.post(
 
 // list of sales
 router.get("/", authenticate, authorize(["cashier:manage"]), saleList);
+
+// individual sale info
+router.get("/:id", authenticate, authorize(["cashier:manage"]), saleInfo);
 
 export default router;
