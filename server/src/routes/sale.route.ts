26e22cb280c89543d1ccdb6b57aa95ee5c37ddb1
SOHEL RANA <sohelrana104322@gmail.com>
2026-03-23 22:10:41 +0600

    update sale route

--- server/src/routes/sale.route.ts
+++ server/src/routes/sale.route.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { authenticate, authorize } from "../middlewares/auth.middleware";
 import { validate } from "../middlewares/validate.middleware";
-import { saleSchemaValidator } from "../validators/sale.validator";
+import {
+  saleSchemaValidator,
+  updateSaleValidator,
+} from "../validators/sale.validator";
 import {
   createSale,
   saleList,
   saleInfo,
+  updateSale,
   deleteSale,
 } from "../controllers/sale.controller";
 
@@ -25,6 +29,15 @@ router.get("/", authenticate, authorize(["cashier:manage"]), saleList);
 
 // individual sale info
 router.get("/:id", authenticate, authorize(["cashier:manage"]), saleInfo);
+
+// update sale
+router.put(
+  "/:id",
+  authenticate,
+  authorize(["cashier:manage"]),
+  validate(updateSaleValidator),
+  updateSale,
+);
 
 // delete sale
 router.delete("/:id", authenticate, authorize(["cashier:manage"]), deleteSale);
