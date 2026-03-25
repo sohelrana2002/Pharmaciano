c146ca7ac3eac392b1b1db4554169da2d879bc38
SOHEL RANA <sohelrana104322@gmail.com>
2026-03-26 01:19:17 +0600

    import parseMedicineInput method

--- server/src/controllers/sale.controller.ts
+++ server/src/controllers/sale.controller.ts
@@ -10,19 +10,7 @@ import Medicine from "../models/Medicine.model";
 import Sale from "../models/Sale.model";
 import { customMessage } from "../constants/customMessage";
 import mongoose from "mongoose";
-
-// Parse medicine input like "napa 500mg" or "napa 500 mg"
-function parseMedicineInput(input: string) {
-  input = input.trim();
-  const match = input.match(/(\d+(?:\.\d+)?)\s*([a-zA-Z]+)/);
-  if (match) {
-    const strength = match[1];
-    const unit = match[2];
-    const name = input.slice(0, match.index).trim();
-    return { name, strength, unit };
-  }
-  return { name: input, strength: "", unit: "" };
-}
+import { parseMedicineInput } from "../helper/parseMedicineInput";
 
 const createSale = async (req: AuthRequest, res: Response) => {
   try {
