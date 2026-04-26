1b709ef667a187b7665452455c82e6eaa902b196
SOHEL RANA <sohelrana104322@gmail.com>
2026-04-26 22:34:08 +0600

    account model schema

---
+++ server/src/models/Account.model.ts
@@ -0,0 +1,42 @@
+import { Schema, model } from "mongoose";
+import { IAccount } from "../types";
+
+const accountSchema = new Schema<IAccount>(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    type: {
+      type: String,
+      enum: ["asset", "liability", "income", "expense", "equity"],
+      required: true,
+    },
+    code: {
+      type: String,
+      required: true,
+    },
+    organizationId: {
+      type: Schema.Types.ObjectId,
+      ref: "Organization",
+      required: true,
+    },
+    branchId: {
+      type: Schema.Types.ObjectId,
+      ref: "Branch",
+      default: null,
+    },
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true },
+);
+
+// prevent duplicate code per organizaton
+accountSchema.index({ code: 1, organizationId: 1 }, { unique: true });
+
+export const Account = model<IAccount>("Account", accountSchema);
