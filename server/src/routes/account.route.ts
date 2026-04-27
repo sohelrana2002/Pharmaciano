31f4681b555d65e3312720323df7274b0759fdd2
SOHEL RANA <sohelrana104322@gmail.com>
2026-04-28 02:36:36 +0600

    individual account info route

--- server/src/routes/account.route.ts
+++ server/src/routes/account.route.ts
@@ -2,7 +2,11 @@ import { Router } from "express";
 import { authenticate, authorize } from "../middlewares/auth.middleware";
 import { validate } from "../middlewares/validate.middleware";
 import { createAccountValidator } from "../validators/account.validator";
-import { accountList, createAccount } from "../controllers/account.controller";
+import {
+  accountInfo,
+  accountList,
+  createAccount,
+} from "../controllers/account.controller";
 
 const router = Router();
 
@@ -21,6 +25,14 @@ router.get(
   authenticate,
   authorize(["account:manage", "superAdmin:manage"]),
   accountList,
+);
+
+// individual account info
+router.get(
+  "/:id",
+  authenticate,
+  authorize(["account:manage", "superAdmin:manage"]),
+  accountInfo,
 );
 
 export default router;
