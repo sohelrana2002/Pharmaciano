d2ae2f68ce2b167af6b6555d53b2cd6b4829b4f6
SOHEL RANA <sohelrana104322@gmail.com>
2026-07-02 03:49:48 +0600

    connectRedis in main file

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,16 @@
 import app from "./src/app";
 import { config } from "./src/config/config";
 import connectDatabase from "./src/config/db.config";
+import { connectRedis } from "./src/config/redis.config";
 
 const PORT = config.port;
 
 const startServer = async (): Promise<void> => {
+  // Connect to MongoDB
   await connectDatabase();
+
+  // Connect to redis globally
+  await connectRedis();
 
   app.listen(PORT, () => {
     console.log(`Listening on port:${PORT}`);
